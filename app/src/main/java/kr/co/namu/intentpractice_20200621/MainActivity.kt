6621fa925534ecc967aae2ed6a626295aafe0a9d
yejun <yejun0890@gmail.com>
2020-06-21 18:04:56 +0900

    다른 화면으로 실제 이동 처리 - intent 메모 참고

--- app/src/main/java/kr/co/namu/intentpractice_20200621/MainActivity.kt
+++ app/src/main/java/kr/co/namu/intentpractice_20200621/MainActivity.kt
@@ -1,11 +1,23 @@
 package kr.co.namu.intentpractice_20200621
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        firstActivityBtn.setOnClickListener {
+            val myIntent = Intent(this, FirstActivity::class.java)
+
+            //실제 인텐트 사용 --> 비행기 탑승 (출발)
+            startActivity(myIntent)
+
+
+        }
     }
 }
