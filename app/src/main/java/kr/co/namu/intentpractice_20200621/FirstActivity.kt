20d55b4802b857a20de53d348df750dd177fba43
yejun <yejun0890@gmail.com>
2020-06-21 18:17:45 +0900

    닫기버튼 누르면 실제 화면 종료 처리  -> intent 를 만들지 않고 finish()로 처리

--- app/src/main/java/kr/co/namu/intentpractice_20200621/FirstActivity.kt
+++ app/src/main/java/kr/co/namu/intentpractice_20200621/FirstActivity.kt
@@ -1,11 +1,23 @@
 package kr.co.namu.intentpractice_20200621
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_first.*
 
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
+
+        closeBtn.setOnClickListener {
+            //메인화면으로 돌아가기 --> 새화면 호풀X, 지금 화면 종료
+
+//            val myIntent = Intent(this,MainActivity::class.java)
+//            startActivity(myIntent)
+
+            finish()
+
+        }
     }
 }
